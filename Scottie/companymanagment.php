f9135be5945d3def96108fa017cc12928851e292
scottiesage <scottie.sage.22@cnu.edu>
2025-02-17 22:11:40 -0500

    created php files & connected to database

---
+++ Scottie/companymanagment.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$userName = "root";
+$passWord = "";
+$dbname = "alumniconnectdb";
+
+// Create connection
+$conn = new mysqli($servername, $userName, $passWord, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
